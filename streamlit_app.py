51739b2b560a8bc5d1025e1c559c61a2f8c7772c
Janetaps08 <145361261+Janetaps08@users.noreply.github.com>
2025-02-20 22:22:35 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,8 +5,8 @@ st.title('online shoppers intenstion')
 st.write('will it create a big revenue')
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
+# import matplotlib.pyplot as plt
+# import seaborn as sns
 df=pd.read_csv['https://github.com/sharmaroshan/Online-Shoppers-Purchasing-Intention/blob/master/online_shoppers_intention.csv']
 df
 
