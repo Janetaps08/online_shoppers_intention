bc4e83d4b52d5b0114a9b617213878f6672a7956
Janetaps08 <145361261+Janetaps08@users.noreply.github.com>
2025-02-20 22:20:38 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-df=pd.read_csv'https://github.com/sharmaroshan/Online-Shoppers-Purchasing-Intention/blob/master/online_shoppers_intention.csv'
+df=pd.read_csv['https://github.com/sharmaroshan/Online-Shoppers-Purchasing-Intention/blob/master/online_shoppers_intention.csv']
 df
 
 df.columns
